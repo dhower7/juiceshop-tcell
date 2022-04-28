5f0f97f755f3751442d513f2659d288be992c1ae
David Howe <david_howe@rapid7.com>
2022-04-28 15:04:44 -0500

    Update app.ts

--- app.ts
+++ app.ts
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: MIT
  */
 
+require('tcell-agent')
+
 require('./lib/startup/validateDependencies')().then(() => {
   const server = require('./server')
   server.start()
